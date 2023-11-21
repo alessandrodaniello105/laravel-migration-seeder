dc36d1c4f1df656dbd1bd1fd848d4218f5104fb8
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-21 18:00:08 +0100

    [add]: print all trains in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,37 @@
     @vite('resources/js/app.js')
 </head>
 <body>
+    <div class="container my-3">
 
+        <table class="table">
+
+            <thead>
+              <tr>
+                <th scope="col">Stazione di Partenza</th>
+                <th scope="col">Orario di Partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">Giorno di Partenza</th>
+                {{-- <td scope="col">Azioni</td> --}}
+              </tr>
+            </thead>
+
+            <tbody>
+
+                @foreach ($trains as $train)
+                <tr>
+                    <td>{{$train->departure_station}}</td>
+                    <td>{{$train->departure_time}}</td>
+                    <td>{{$train->arrival_station}}</td>
+                    <td>{{$train->arrival_time}}</td>
+                    <td>{{$train->departure_date}}</td>
+                    {{-- <td>Azioni</td> --}}
+                </tr>
+                @endforeach
+
+            </tbody>
+          </table>
+
+    </div>
 </body>
 </html>
